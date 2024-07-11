9decf4c144da1d20498341db2c57566f727e2516
Puja1604 <pujag1691@gmail.com>
2024-07-11 13:06:02 -0400

    Initial Commit

---
+++ Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by Puja Gogineni on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
